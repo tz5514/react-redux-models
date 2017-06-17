e9f3cae45e19d11d77b6f09b042cc0942bed41de
tz5514 <tz5514@gmail.com>
2017-06-17 10:57:01 +0800

    version 0.5.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import combineModelReducers from './combineModelReducers'
 import connectModel from './connectModel'
-import wrapProvider from './wrapProvider'
+import wrapWithProvider from './wrapWithProvider'
 import createActionTypes, { AJAX_ACTION_TYPES } from './createActionTypes'
 import createStore from './createStore'
 import Model from './Model'
 
 export {
-  wrapProvider,  
+  wrapWithProvider,  
   createStore,  
   combineModelReducers,
   connectModel, 
