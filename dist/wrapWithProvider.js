95317da36c332b560d9c8eee1675baaba8d9a593
tz5514 <tz5514@gmail.com>
2017-06-17 11:56:06 +0800

    0.6.0

--- dist/wrapWithProvider.js
+++ dist/wrapWithProvider.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -71,10 +75,10 @@ var wrapWithProvider = function wrapWithProvider(storeOptions) {
       }], [{
         key: 'getInitialProps',
         value: function () {
-          var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+          var _ref = _asyncToGenerator(_regenerator2.default.mark(function _callee() {
             var ctx = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
             var initialProps;
-            return regeneratorRuntime.wrap(function _callee$(_context) {
+            return _regenerator2.default.wrap(function _callee$(_context) {
               while (1) {
                 switch (_context.prev = _context.next) {
                   case 0:
